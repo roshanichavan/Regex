c6e12a63883755e08c0c341289e2222743908d68
roshani chavan <roshanic355@gmail.com>
2022-11-24 19:24:33 +0530

    Added:Regex_1

---
+++ ForFirstNumber.java
@@ -0,0 +1,26 @@
+package day18;
+
+	import java.util.Scanner;
+	import java.util.regex.*;
+	public class ForFirstNumber {
+
+
+
+		public static void main(String[] args) {
+			// First name starts with Cap and has minimum 3 characters
+			Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+			try (Scanner sc = new Scanner(System.in)) {
+				System.out.println("Enter the First Name");
+				String input = sc.next();
+				Matcher matcher = pattern.matcher(input);
+				boolean matchFound = matcher.find();
+				if (matchFound)
+					System.out.println("Valid entry");
+				else
+					System.out.println("Invalid entry");
+			}
+
+		}
+	}
+
+
---
+++ ValidEmail.java
@@ -0,0 +1,23 @@
+package day18;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class ValidEmail {
+
+	public static void main(String[] args) {
+		Pattern pattern = Pattern.compile("^(abc)[.]?[a-z]*@(bl.co)[.]?[a-z]{0,2}$");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("enter the email");
+			String input = sc.next();
+			Matcher matcher = pattern.matcher(input);
+			boolean matchFound = matcher.find();
+			if (matchFound)
+				System.out.println("Valid entry");
+			else
+				System.out.println("Invalid entry");
+		}
+
+	}
+}
+
